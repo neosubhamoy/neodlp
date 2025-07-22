b0b65555328b6894a6c0d5edae55a14f336f75ba
Subhamoy Biswas <subhamoybiswas636@gmail.com>
2025-07-22 10:18:42 +0530

    chore: bumped up deps

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,9 @@ const host = process.env.TAURI_DEV_HOST;
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react(), tailwindcss()],
-
+  build: {
+    chunkSizeWarningLimit: 1024, // 1MB
+  },
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
