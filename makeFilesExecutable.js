ba5df1e3e6aee1300728f9c3abe3d2d73e95b18c
Subhamoy Biswas <subhamoybiswas636@gmail.com>
2025-08-11 22:09:13 +0530

    refactor: using ffmpeg as sidecar, bumped up ffmpeg to 7.1.1 and added ffprobe

--- makeFilesExecutable.js
+++ makeFilesExecutable.js
@@ -9,7 +9,6 @@ const __dirname = path.dirname(__filename);
 // Define array of binary source directories
 const binSrcDirs = [
     path.join(__dirname, 'src-tauri', 'binaries'),
-    path.join(__dirname, 'src-tauri', 'resources', 'binaries'),
 ];
 
 function makeFilesExecutable() {
