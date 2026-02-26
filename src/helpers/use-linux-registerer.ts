5e90fac99f92c98bc1e70035857899e503aad0e0
Subhamoy Biswas <subhamoybiswas636@gmail.com>
2026-02-26 13:44:51 +0530

    refactor: use cat instead of echo

--- src/helpers/use-linux-registerer.ts
+++ src/helpers/use-linux-registerer.ts
@@ -63,7 +63,7 @@ export function useLinuxRegisterer() {
                         console.log(`File ${file.source} copied successfully to ${destinationPath}`);
                         LOG.info("LINUX REGISTERER", `File ${file.source} copied successfully to ${destinationPath}`);
                         if (file.content) {
-                            const writeCommand = Command.create('echo', [file.content, '>', destinationPath]);
+                            const writeCommand = Command.create('cat', ['>', destinationPath, '<<', 'EOF', file.content, 'EOF']);
                             const writeOutput = await writeCommand.execute();
                             if (writeOutput.code === 0) {
                                 console.log(`Content written successfully to ${destinationPath}`);
