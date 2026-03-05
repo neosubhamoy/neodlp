b96d8fe64b669be4e5ad661f5a302259252f241f
Subhamoy Biswas <subhamoybiswas636@gmail.com>
2026-03-05 15:26:05 +0530

    fix: neodlp-pot sidecar not registered error on flatpak

--- src/helpers/use-pot-server.ts
+++ src/helpers/use-pot-server.ts
@@ -17,7 +17,12 @@ export default function usePotServer() {
     };
 
     const startPotServer = async (port?: number) => {
-        const runCommand = Command.sidecar('binaries/neodlp-pot', [
+        const isFlatpak = await invoke<boolean>('is_flatpak');
+        const runCommand = isFlatpak
+        ? Command.create('sh', [
+            '-c', `/app/bin/neodlp-pot server --port ${port ? port.toString() : potServerPort.toString()}`
+        ])
+        : Command.sidecar('binaries/neodlp-pot', [
             'server',
             '--port',
             port ? port.toString() : potServerPort.toString(),
